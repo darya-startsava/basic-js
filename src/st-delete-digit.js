d3a05c8436899e35317fe7e4a92cc93c02775c92
darya-startsava <daryastartsava@gmail.com>
2021-09-29 14:37:59 +0200

    feat: add solution for delete-digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,11 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+    n = n.toString();
+    for (let i = 0; i < n.length - 1; i++) {
+        if (n[i] < n[i + 1]) {
+            return +(n.substring(0, i) + n.slice(i + 1));
+        }
+    } return +n.substring(0, n.length - 1);
 }
